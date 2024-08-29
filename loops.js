fb83ccf0aaada8145cc6df24e0aa1cf85cdcf0bf
daniel lopez <149839672+qw-label@users.noreply.github.com>
2024-08-29 10:38:59 -0500

    working with loops

---
+++ loops.js
@@ -0,0 +1,19 @@
+// for(let i = 0; i < 5; i++) {
+//     console.log(`This is iteration ${i}`);
+// }
+
+// const cars = ['ford', 'toyota', 'chevy'];
+// for(let i = 0; i < cars.length; i++) {
+//     console.log(cars[i].toUpperCase());
+// }
+
+// const names = ['mario', 'luigi', 'yoshi'];
+// names.forEach(name => {
+//     console.log(name.toUpperCase());
+// });
+
+let i = 0;
+while(i < 10) {
+    console.log(`is i less than 10? ${i}`);
+    i++;    
+}
